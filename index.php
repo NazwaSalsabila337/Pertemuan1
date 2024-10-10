1ffc860749f1385e92a12c0308eabfd6900ba0e5
NazwaSalsabila337 <nazwasalsabilla017@gmail.com>
2024-10-10 09:31:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh Varibel PHP</title>
+</head>
+<?php
+    $nama = "Nazwa Salsabila";
+    $umur = 18;
+    ?>
+
+<body>
+    <h1>Hallo, <?php echo $nama; ?>!</h1>
+    <p>Umur Anda adalah <?php echo $umur; ?> tahun.</p>
+
+</body>
+</html>
---
+++ logika.php
@@ -0,0 +1,10 @@
+<?php 
+$nilai = 80; 
+$umur = 25; 
+  
+if ($nilai >= 20 && $umur > 18) { 
+echo "Lulus ujian dan memiliki usia di atas 18 tahun."; 
+} else { 
+echo "Tidak memenuhi syarat."; 
+} 
+?>
